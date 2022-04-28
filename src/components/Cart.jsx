9eba6a854b08a9ce9e772b8e5480fc481d9de84f
programer1983 <programer972@gmail.com>
2022-04-28 12:45:30 +0300

    commit-6;

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,9 +1,49 @@
 import {useState, useEffect} from "react"
 import "./../Styles/Cart.css"
 
-const Cart = () => {
+const Cart = ({cart, setCart, handleChange}) => {
+    const [price, setPrice] = useState(0)
+
+    const handleRemove = (id) => {
+        const arr = cart.filter((item) => item.id !== id)
+        setCart(arr)
+        handlePrice()
+    }
+
+    const handlePrice = () => {
+        let ans = 0
+        cart.map((item) => (ans + item.amount * item.price))
+    }
+
+    useEffect(() => {
+        handlePrice()
+    })
+  
+
   return (
-    <div>Cart</div>
+    <article>
+        {cart.map((item) => (
+            <div className="cart__box" key={item.id}>
+                <div className="cart__img">
+                    <ing src={item.img} alt="" />
+                    <p>{item.title}</p>
+                </div>
+                <div>
+                    <button onClick={() => handleChange(item, 1)}></button>
+                    <button>{item.amount}</button>
+                    <button onClick={() => handleChange(item, -1)}></button>
+                </div>
+                <div>
+                    <span>{item.price}</span>
+                    <button onClick={() => handleRemove(item.id)}>Remove</button>
+                </div>
+            </div>
+        ))}
+        <div className="total">
+            <span>Total Price of your Cart</span>
+            <span>Rs - {price}</span>
+        </div>
+    </article>
   )
 }
 
